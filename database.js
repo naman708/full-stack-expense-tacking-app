11ef86e5df79ce7d7991d10859cf6ada17b8e60c
naman708 <83646450+naman708@users.noreply.github.com>
2023-10-31 22:50:25 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,4 +1,4 @@
 
 const { Sequelize } = require('sequelize');
-const sequelize=new Sequelize('node_com','root','Naman@11',{dialect:'mysql',host:'localhost'});
+const sequelize=new Sequelize('','root','',{dialect:'mysql',host:'localhost'});
 module.exports=sequelize;
